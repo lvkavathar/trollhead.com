5ea0c2e9e73a2cc72bc20ee2c1d1bbd781409ea3
venkat-lingala <lvenkatkumarreddy@gmail.com>
2017-06-11 15:28:13 -0400

    Refactor home page view to use a template

--- infocool/settings.py
+++ infocool/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'lists',
 ]
 
 MIDDLEWARE = [
@@ -50,11 +51,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'infocool.urls'
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(SETTINGS_PATH, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- lists/tests.py
+++ lists/tests.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 # Create your tests here.
 
@@ -14,9 +15,9 @@ class HomePageTest(TestCase):
 		self.assertEqual(found.func, home_page)
 	
 	def test_home_page_returns_correct_html(self):
-		request = HttpRequest()
-		response = home_page(request)
+		response = self.client.get('/')
 		html = response.content.decode('utf8')
 		self.assertTrue(html.startswith('<html>'))
 		self.assertIn('<title>To-Do lists</title>', html)
 		self.assertTrue(html.endswith('</html>'))
+		self.assertTemplateUsed(response, 'home.html')
